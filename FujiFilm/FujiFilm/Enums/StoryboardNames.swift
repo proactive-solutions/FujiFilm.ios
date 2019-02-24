b2141d8dc944a14605f82dc528123313f41073ad
proactive-solutions <mailme.pksharma@gmail.com>
2019-02-24 14:51:13 +0530

    Add fonts in xcode project

--- FujiFilm/FujiFilm/Enums/StoryboardNames.swift
+++ FujiFilm/FujiFilm/Enums/StoryboardNames.swift
@@ -6,39 +6,32 @@ import Foundation
 import UIKit
 
 enum StoryBoardNames: String {
-    case onboarding = "Onboarding"
-    case termsOfUse = "TermsOfUse"
-    case privacyPolicy = "PrivacyPolicy"
-    case confirmSeedSafety = "ConfirmSeedSafety"
-    case createAccount = "CreateAccount"
-    case createNewPassword = "CreateNewPassword"
-    case seedPhrase = "SeedPhrase"
-    case welcome = "Welcome"
-    case accountExists = "AccountExists"
-    case restoreFromSeed = "RestoreFromSeed"
-    case home = "Home"
-    case welcomeHome = "WelcomeHome"
-    case dashboard = "Dashboard"
-    case purchasePMA = "PurchasePMA"
-    case wallet = "Wallet"
-    case transactionHistory = "TransactionHistory"
-    case transactionFilter = "TransactionFilter"
-    case receiveFunds = "ReceiveFunds"
-    case walletSettings = "WalletSettings"
-    case supportedCurrencies = "SupportedCurrencies"
-    case qrCode = "QRCode"
-    case sendFund = "SendFund"
-    case amount = "Amount"
-    case walletDetails = "WalletDetails"
+    case onboarding = "Login"
 }
 
 extension StoryBoardNames {
     var controller: String {
-        return rawValue + "VC"
+        return rawValue + "ViewController"
+    }
+
+    private var initilControllerNotSet: String {
+        return "InitilControllerNotSet in storyboard"
     }
 
     private var controllerInstantiageErrorMessage: String {
         return "Could not instantiate controller. Please check storyboard identifier"
+    }
+
+    func initialController() -> UIViewController {
+        let storyboard = UIStoryboard(
+            name: rawValue,
+            bundle: nil
+        )
+
+        guard let controller = storyboard.instantiateInitialViewController() else {
+            fatalError(initilControllerNotSet)
+        }
+        return controller
     }
 
     func instatiateController() -> UIViewController {
@@ -50,16 +43,6 @@ extension StoryBoardNames {
         let controller = storyboard.instantiateViewController(
             withIdentifier: self.controller
         )
-        return controller
-    }
-
-    func instatiateControllerWithIdentifier(name: String) -> UIViewController {
-        let storyboard = UIStoryboard(
-            name: rawValue,
-            bundle: nil
-        )
-
-        let controller = storyboard.instantiateViewController(withIdentifier: name)
         return controller
     }
 }
