bce058aa60d38ea8c004ab8c93a15a0749bac029
proactive-solutions <mailme.pksharma@gmail.com>
2019-05-05 20:22:02 +0530

    Increase build number and change JSON handling for the login response

--- FujiFilm/FujiFilm/models/UserDetails.swift
+++ FujiFilm/FujiFilm/models/UserDetails.swift
@@ -16,6 +16,8 @@ struct UserDetails: Codable {
 
     struct Result: Codable {
         let fldDid: String
+        let fldParentID: String
+        let fldRole: String
         let fldSlug: String
         let fldFirstname: String
         let fldFamilyname: String
@@ -27,20 +29,17 @@ struct UserDetails: Codable {
         let fldState: String
         let fldCity: String
         let fldArea: String
-        let fldAddress: String
         let fldAddress1: String
+        let fldAddress2: String
         let fldLandmark: String
         let fldZipcode: String
-        let fldPostalcode: String
         let fldLat: String
         let fldLong: String
-        let fldActivationlink: String
+        let fldCurrency: String
         let fldStatus: String
         let fldUserType: String
         let fldCreatedBy: String
-        let fldParentID: String
-        let fldCurrency: String
-        let fldCreatedat: String
+        let fldCreated: String
 
         var fullName: String {
             return (fldFirstname + " " + fldFamilyname)
@@ -48,6 +47,8 @@ struct UserDetails: Codable {
 
         enum CodingKeys: String, CodingKey {
             case fldDid = "fld_did"
+            case fldParentID = "fld_parent_id"
+            case fldRole = "fld_role"
             case fldSlug = "fld_slug"
             case fldFirstname = "fld_firstname"
             case fldFamilyname = "fld_familyname"
@@ -59,20 +60,17 @@ struct UserDetails: Codable {
             case fldState = "fld_state"
             case fldCity = "fld_city"
             case fldArea = "fld_area"
-            case fldAddress = "fld_address"
-            case fldAddress1 = "fld_address1"
+            case fldAddress1 = "fld_address_1"
+            case fldAddress2 = "fld_address_2"
             case fldLandmark = "fld_landmark"
             case fldZipcode = "fld_zipcode"
-            case fldPostalcode = "fld_postalcode"
             case fldLat = "fld_lat"
             case fldLong = "fld_long"
-            case fldActivationlink = "fld_activationlink"
+            case fldCurrency = "fld_currency"
             case fldStatus = "fld_status"
             case fldUserType = "fld_user_type"
             case fldCreatedBy = "fld_created_by"
-            case fldParentID = "fld_parent_id"
-            case fldCurrency = "fld_currency"
-            case fldCreatedat = "fld_createdat"
+            case fldCreated = "fld_created"
         }
     }
 }
